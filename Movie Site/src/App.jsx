7d89969d2aa824b99b84d4fdec9c117e37249ed2
hadi.prms <hadi.pourmohammad1383@gmail.com>
2024-11-17 11:53:47 +0330

    rendered components correct way, move them to main

--- Movie Site/src/App.jsx
+++ Movie Site/src/App.jsx
@@ -2,23 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Fetcher from './mainPageComponents/searchBar'
-import TopRatedMovies from './mainPageComponents/MostPopularMovies'
-import TopRatedSeries from './mainPageComponents/recentlyUpdatedSeries'
-import Footer from './mainPageComponents/footer'
-import MovieSlider from './mainPageComponents/MovieSlider'
-import { SkeletonTheme } from 'react-loading-skeleton'
+import Main from './main'
 
 function App() {
 
   return (
     <>
-      <SkeletonTheme baseColor="#313131" highlightColor="#525252">
-        <MovieSlider />
-        <TopRatedMovies />
-        {/* <TopRatedSeries /> */}
-        <Footer />
-      </SkeletonTheme>
+    <Main />
     </>
   )
 }
--- Movie Site/src/main.jsx
+++ Movie Site/src/main.jsx
@@ -2,6 +2,29 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Fetcher from './mainPageComponents/searchBar'
+import TopRatedMovies from './mainPageComponents/MostPopularMovies'
+import TopRatedSeries from './mainPageComponents/recentlyUpdatedSeries'
+import Footer from './mainPageComponents/footer'
+import MovieSlider from './mainPageComponents/MovieSlider'
+import { SkeletonTheme } from 'react-loading-skeleton'
+
+function Main() {
+
+  return (
+    <>
+      <SkeletonTheme baseColor="#313131" highlightColor="#525252">
+        <MovieSlider />
+        <TopRatedMovies />
+        {/* <TopRatedSeries /> */}
+        <Footer />
+      </SkeletonTheme>
+    </>
+  )
+}
+
+export default Main;
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
